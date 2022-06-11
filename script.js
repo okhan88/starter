6d0a499cb075d9f33d72b4dba10bfe651f78a5f6
osama <okpro333@gmail.com>
2022-06-11 06:39:13 -0400

    coding challenge 2

--- script.js
+++ script.js
@@ -185,3 +185,42 @@ console.log (jonasNew)
 
 */
 // --------------------------------------------------------------
+
+// taking decisions: if/else statements
+
+/*
+const age = 15
+// const isOldEnough = age >= 18 
+
+if(age >= 18) {
+    console.log('Sarah can start driving license')
+} else {
+    const yearsLeft = 18 - age
+    console.log(`you have ${yearsLeft} years left!`)
+}
+
+const birthYear = 2010
+let century;
+
+if(birthYear <= 2000) {
+    century = 20
+} else {
+    century = 21
+}
+
+console.log(century)
+
+*/
+
+// --------------------------------------------------------------
+/* coding challenge 2
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
+*/
